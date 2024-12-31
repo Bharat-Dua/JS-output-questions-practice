6ec3b9071aee6a1d08d61ee29a67840b8c3b64d8
Bharat-Dua <bharatdua13@gmail.com>
2025-01-01 01:18:31 +0530

    solved comparison problem

--- 07-problem.js
+++ 07-problem.js
@@ -1,5 +1,5 @@
 let a = 108;
-let b = new Number(108);
+let b = new Number(108); // it's number constructor and type of object
 let c = 108;
 console.log(a == b); // true
 console.log(a === b); // false
