61dc447195781d142447d9d4fbac3a28fa8f17a5
Bharat-Dua <bharatdua13@gmail.com>
2025-01-01 01:48:51 +0530

    solved best practice to error free code problem

---
+++ 09-problem.js
@@ -0,0 +1,4 @@
+"use strict";
+let message;
+masage = { data: [24] };
+console.log(masage); // will print data and here you can see we misspell the variable name so js by default will create a new variable with the misspelled name with global scope to prevent from these kind of mistakes just use the 'use strict' directive at the top of your script now it's will throw an error
