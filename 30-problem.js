215c876427c08e148de6f343f4c4d19fd8cc9684
Bharat-Dua <bharatdua13@gmail.com>
2025-01-01 17:08:57 +0530

    solved IIFE problem

---
+++ 30-problem.js
@@ -0,0 +1,13 @@
+(() => {
+  let x, y;
+  try {
+    throw new Error();
+  } catch (x) {
+    (x = 1), (y = 2);
+    console.log(x); // 1
+  }
+  console.log(x); // undefined
+  console.log(y); // 2
+})();
+
+//* local scope always have higher priority
