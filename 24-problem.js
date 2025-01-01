a4ea6969611bb709ec2fed3a0c58a94a541fa1c9
Bharat-Dua <bharatdua13@gmail.com>
2025-01-01 13:23:25 +0530

    solved prototype problem

---
+++ 24-problem.js
@@ -0,0 +1,8 @@
+String.prototype.youAreAmazing = () => {
+  return "You are amazing my friend";
+};
+
+const who = "Bharat";
+console.log(who.youAreAmazing()); // Output: You are amazing my friend
+
+//* Prototype:- Prototype is a key,that exists on every object and function and its value of prototype is object.and we use prototype to share common functionality among object instances.
