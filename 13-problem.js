773555362effd4a63618eec61f479cbc875a4a49
Bharat-Dua <bharatdua13@gmail.com>
2025-01-01 10:43:28 +0530

    solved event propagation problem

---
+++ 13-problem.js
@@ -0,0 +1,6 @@
+//? what are the three phases of event propagation?
+
+//* A: Target > Capturing > Bubbling
+//* D: Target > Bubbling > Capturing
+//* B: Capturing > Target > Bubbling <- Correct order
+//* C: Bubbling > Capturing > Target
