2143e6edbba4883b27f17e7089b1b168eeb5310a
Bharat-Dua <bharatdua13@gmail.com>
2025-01-02 14:46:05 +0530

    solved object des problem

---
+++ 47-problem.js
@@ -0,0 +1,3 @@
+const { fName: feDev } = { fName: "Mr.X" };
+
+console.log(fName); // Reference Error: fName is not defined bcoz here we are using object des and we are renaming the prop name and now we are trying to access the older prop.
