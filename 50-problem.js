306f5ed3a00183557cc904201d8edc063753b51a
Bharat-Dua <bharatdua13@gmail.com>
2025-01-02 15:09:21 +0530

    solved push problem

---
+++ 50-problem.js
@@ -0,0 +1,4 @@
+let newList = [1, 2].push(4); // 3
+console.log(newList.push(5)); // newList.push is not a function bcoz here we are trying to push into integer rather than array
+
+//* Bcoz return value of push in js is no of elements.
