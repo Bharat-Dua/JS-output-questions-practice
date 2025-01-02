2701a9ffde3a161f3f7dde1e4bb20e5231fb0a4f
Bharat-Dua <bharatdua13@gmail.com>
2025-01-02 15:39:16 +0530

    solved return keyword problem

---
+++ 51-problem.js
@@ -0,0 +1,9 @@
+function nums(a, b) {
+  if (a > b) console.log("a is large"); // 'a is large'
+  else console.log("b is large"); // 'b is large'
+  return;
+  a + b;
+}
+
+console.log(nums(4, 2)); // undefined
+console.log(nums(1, 2)); // undefined
