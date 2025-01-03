4e53b6f0f3d857347c380a908da31c1a58993a4f
Bharat-Dua <bharatdua13@gmail.com>
2025-01-03 21:01:46 +0530

    solved toReversed() problem

---
+++ 84-problem.js
@@ -0,0 +1,8 @@
+const data1 = ["C", "B", "A"];
+const data2 = ["Z", "Y", "X"];
+
+data1.toReversed(); // The toReversed() method in JavaScript creates a reversed copy of an array without altering the original array.
+data2.reverse();
+
+console.log({ data1 }); // { data1: [ 'C', 'B', 'A' ] }
+console.log({ data2 }); // { data2: [ 'X', 'Y', 'Z' ] }
