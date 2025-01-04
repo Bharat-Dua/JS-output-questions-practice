a2a81a92afe8d8a5af91f390db32e92404f7507b
Bharat-Dua <bharatdua13@gmail.com>
2025-01-04 10:08:21 +0530

    solved string problem

---
+++ 91-problem.js
@@ -0,0 +1,4 @@
+let str = new String("JS");
+
+console.log(str === "JS"); // false bcoz str is an object and "JS" is a string and constructor function is always typeof object.
+console.log(str == "JS"); // true
