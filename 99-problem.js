71e3e2b2d4344b8b9b1fc43a8e5c523cc53d7b28
Bharat-Dua <bharatdua13@gmail.com>
2025-01-04 15:24:05 +0530

    solved toString problem

---
+++ 99-problem.js
@@ -0,0 +1,4 @@
+
+console.log((3)["toString"].length + 1); // 2 bcoz here we are not calling just accessing the method property and The length property of a function returns the number of arguments the function expects. The toString method of a Number object expects one optional argument (the radix), so 3.toString.length is 1.
+
+//* function.length returns count of arguments
