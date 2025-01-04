60fdf742ee27fb4f57082acc821c7da441dad23d
Bharat-Dua <bharatdua13@gmail.com>
2025-01-04 12:43:11 +0530

    solved array problem

---
+++ 94-problem.js
@@ -0,0 +1,2 @@
+const arr = [1, 2, 3];
+console.log(arr[5]); // undefined bcoz as we know array is also object in js and here we are trying to access the key at certain index and if we try to access the key which doesn't exist in obj we get undefined.
