60fb404e71186f973e0c32186c6b3dade3de8269
Bharat-Dua <bharatdua13@gmail.com>
2025-01-04 12:48:02 +0530

    solved default param problem

---
+++ 95-problem.js
@@ -0,0 +1,8 @@
+function init(x, y, z) {}
+
+function end(a, b = 0, c) {}
+
+console.log(init.length); //3
+console.log(end.length); // 1
+
+//* Note :- if we give any param default value if doesn't cound and after that param element also.
