61a5b3b30f523303fe3d50fcc8333b4938c1ef6c
Bharat-Dua <bharatdua13@gmail.com>
2025-01-05 13:56:25 +0530

    solved number problem

---
+++ 112-problem.js
@@ -0,0 +1,5 @@
+console.log(888888888888888888); // 888888888888888800
+
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+
+//* if we try to print the number which is bigger than MAX_SAFE_INTEGER, it will be rounded down to MAX_SAFE_INTEGER and add the 0 to end so use bigint
