7bbe200e3cc0f2c41299ae06e701da37dfc7f5b0
Bharat-Dua <bharatdua13@gmail.com>
2025-01-07 18:57:20 +0530

    solved number problem

---
+++ 144-problem.js
@@ -0,0 +1,5 @@
+const num1 = 1000000000;
+
+const num2 = 10_000_000_00;
+
+console.log(num1 === num2); // true bcoz here we're using ES6 concept number seprator where we write a number in chunks to read it easily.
