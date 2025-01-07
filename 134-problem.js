02bed97b50f3a45d09399dc9949467eccf377f2f
Bharat-Dua <bharatdua13@gmail.com>
2025-01-07 15:21:26 +0530

    solved IIFE problem

---
+++ 134-problem.js
@@ -0,0 +1,5 @@
+!(function () {
+  console.log("i m amazed"); // 'i m amazed'
+})();
+
+//* Note :- By default, a function keyword defines a function declaration, which cannot be invoked directly.The ! operator (or any other operator like +, -, ~, etc.) changes the function declaration into a function expression, allowing it to be executed immediately.
