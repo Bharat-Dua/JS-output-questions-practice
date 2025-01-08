5bd04853d2a703df66ef0742e8df5133858e3015
Bharat-Dua <bharatdua13@gmail.com>
2025-01-08 17:32:55 +0530

    solved function problem

---
+++ 164-problem.js
@@ -0,0 +1,7 @@
+const show = (b, b) => {};
+show(); // SyntaxError: Duplicate parameter name not allowed in this context
+
+function test(a, a) {}
+test();
+
+//* Note :- In a regular function we can have duplicate param name ,but in arrow function we can't have .
