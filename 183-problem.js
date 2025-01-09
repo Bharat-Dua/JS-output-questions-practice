42ab56d81170fedc77db76a2afec4d8df7f06f92
Bharat-Dua <bharatdua13@gmail.com>
2025-01-09 20:43:03 +0530

    solved promise problem

---
+++ 183-problem.js
@@ -0,0 +1,14 @@
+(function () {
+  Promise.resolve().then(() => {
+    console.log("A");
+    Promise.resolve().then(() => {
+      console.log("B");
+    });
+  });
+})();
+
+const id = setTimeout(() => {
+  console.log("C");
+}, 0);
+
+// output :- A B C
