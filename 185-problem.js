7ecc3526f23c88a48bebccf2e0685c37a1e128a8
Bharat-Dua <bharatdua13@gmail.com>
2025-01-09 21:32:52 +0530

    solved IIFE problem

---
+++ 185-problem.js
@@ -0,0 +1,21 @@
+// Give 4 ways to create an IIFE
+
+//* method 1
+!(function () {
+  console.log("IIFE"); // 'IIFE'
+})();
+
+//* method 2
++(function () {
+  console.log("IIFE"); // 'IIFE'
+})();
+
+//* method 3
+(function () {
+  console.log("IIFE"); // 'IIFE'
+})();
+
+//* method 4
+(function () {
+  console.log("IIFE"); // 'IIFE'
+})();
