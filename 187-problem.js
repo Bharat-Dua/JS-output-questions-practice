f19b21a70847d9164eb9fe8aacf81b707c4f99c4
Bharat-Dua <bharatdua13@gmail.com>
2025-01-09 22:01:08 +0530

    solved Number problem

---
+++ 187-problem.js
@@ -0,0 +1,4 @@
+console.log(01); // 1 JS will ignore the zero .Octal literals are not allowed. Use the syntax '0o1'.
+console.log(String(01)); // '1' Octal literals are not allowed. Use the syntax '0o1'.
+
+//* As we know there are number system :- Binary,Decimal,hexadecimal,octal 
