c2aa7e260ad6750eb295600c9f26d5bfaf7aa54d
Bharat-Dua <bharatdua13@gmail.com>
2025-01-10 11:18:07 +0530

    solved new keyword problem

---
+++ 193-problem.js
@@ -0,0 +1,7 @@
+// Call show function without using any ()
+
+function show() {
+  console.log("hello world"); // 'hello world'
+}
+
+new show(); // This will call the show function without using ()
