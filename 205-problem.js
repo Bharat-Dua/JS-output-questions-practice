96fa2ce1d5c0b67f6084bc66e530f6bef0d7b174
Bharat-Dua <bharatdua13@gmail.com>
2025-01-10 17:40:36 +0530

    solved type coercion problem

---
+++ 205-problem.js
@@ -0,0 +1,5 @@
+console.log(null == 0); // false bcoz if we compare null except undefined we always get false.
+console.log(null > 0); // false bcoz null ->0 >0
+console.log(null >= 0); // true  bcoz null ->0==0
+
+//* Note:- greater (>) -> converts variable to number then compare
