9b9b48f3fefac0ed8d75df403cc0bfdd5633f355
Bharat-Dua <bharatdua13@gmail.com>
2025-01-11 18:26:46 +0530

    solved destructuring problem

---
+++ 225-problem.js
@@ -0,0 +1,6 @@
+let { x, x: y } = { x: 2 };
+
+console.log("x=", x); // 2
+console.log("y=", y); // 2
+
+//* Note:- In JS we can destructure the same key as many as times but we have to give alter name on every use we can't use same key again.
