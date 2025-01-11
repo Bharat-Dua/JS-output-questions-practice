7c4fab4793628e4ad5caf9edd509eff4d4ca7465
Bharat-Dua <bharatdua13@gmail.com>
2025-01-11 15:34:54 +0530

    solved boxing problem

---
+++ 220-problem.js
@@ -0,0 +1,13 @@
+console.log(2.awesome)// it will throw an error bcoz js engine will gets confused here 1. are we trying to access prop 2. are we trying to get some value after decimal
+
+//* 3 ways to sort 
+
+1. 
+console.log((2).awesome); // undefined
+
+2. 
+console.log(2..awesome); // undefined
+
+3. 
+let num=2;
+console.log(num.awesome) // undefined
