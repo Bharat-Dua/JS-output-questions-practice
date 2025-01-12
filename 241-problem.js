b2e8ec1fde7de1904b7ea3c00b3c08c6f2e2e65a
Bharat-Dua <bharatdua13@gmail.com>
2025-01-12 21:27:29 +0530

    solved OR operator problem

--- 241-problem.js
+++ 241-problem.js
@@ -1,8 +1,15 @@
+// find a bug
+// here even when we pass marks value 0 we are getting 'marks absent instead of just 0
 function showMessage(marks) {
   const message = marks || "Absent";
   console.log(`Marks ${message}`);
 }
 
+//* to resolve this we can use nullish collision(??) . it basically check if any value is null and undefined on left side then it will execute the right side.
+function showMessage(marks) {
+  const message = marks ?? "Absent";
+  console.log(`Marks ${message}`);
+}
 showMessage(20); // `Marks 20'
-showMessage(0); // 'Marks Absent'
+showMessage(0); // 'Marks 0'
 showMessage(46); // 'Marks 46'
