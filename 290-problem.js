eba2df61d3bb6cae95303536a0d0ce3e7d9267b6
Bharat-Dua <bharatdua13@gmail.com>
2025-01-15 19:47:43 +0530

    solved boxing problem

---
+++ 290-problem.js
@@ -0,0 +1,5 @@
+const { length } = "JS";
+
+console.log(length); // 2
+
+//* Bcoz of boxing whenever we try to access some prop/method on primitive value JS BTS create a object respect to their data type and access the prop/method from its prototype and returns the value and after returning the value it destroy them and garbage collector collect them.
