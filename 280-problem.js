f2472c53ebe7d85508a4841bc792134b781ec043
Bharat-Dua <bharatdua13@gmail.com>
2025-01-15 13:58:14 +0530

    solved window object problem

---
+++ 280-problem.js
@@ -0,0 +1,5 @@
+var name = 2024;
+
+console.log(name + 1); // '20241'
+
+//* bcoz in window object there is a reserved keyword with name property so if we try to create a variable in global scope with name and whatever we pass to that variable will gets converted into string.
