e2266865eef0838b639b59844463e7971c92bd00
Bharat-Dua <bharatdua13@gmail.com>
2025-01-15 13:00:52 +0530

    solved type coercion problem

---
+++ 276-problem.js
@@ -0,0 +1,11 @@
+console.log(false == [[[[[[[0]]]]]]]); // true
+
+/*
+false(primitive)== [[[[[[[0]]]]]]] (Non-primitive)
+
+false == [[[[[[[0]]]]]]].toString();
+false == '0'
+Number(false)== Number('0')
+0==0 ->true
+
+*/
