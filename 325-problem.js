b5b998d820157077fcd8f1f2465d5718c93ce507
Bharat-Dua <bharatdua13@gmail.com>
2025-01-17 12:38:45 +0530

    solved substring problem

---
+++ 325-problem.js
@@ -0,0 +1,5 @@
+let x = "learn";
+
+console.log(x.substring(5, 1)); // 'earn'
+
+//* Substring : takes two parameters, start and end index. It returns a string that includes the characters from the start index up to but not including the end index. if the start index is greater than end index it will automatically swap them.
